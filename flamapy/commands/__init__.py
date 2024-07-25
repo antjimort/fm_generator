e56ac83b0ee17c37a677b776b51b6acaf4c17951
José A. Galindo <jagalindo@us.es>
2024-07-25 13:13:48 +0200

    fix: new facade developed, also new simplier cmd

--- flamapy/commands/__init__.py
+++ flamapy/commands/__init__.py
@@ -6,7 +6,7 @@ from functools import wraps
 import inspect
 from pathlib import Path
 from shutil import copytree
-from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
+from flamapy.interfaces.python.flamapy_feature_model import FLAMAFeatureModel
 # List to store registered commands and their arguments
 MANUAL_COMMANDS = []
 
@@ -15,7 +15,7 @@ def command(name, description, *args):
 
     def decorator(func):
         MANUAL_COMMANDS.append((name, description, func, args))
-        
+
         @wraps(func)
         def wrapper(*func_args, **func_kwargs):
             return func(*func_args, **func_kwargs)
@@ -36,8 +36,10 @@ def extract_commands(cls):
     return commands
 
 
-@command('generate_plugin', 'This command generates a new plugin to implement your cusom operations. \
-         To execute it you should set yourself in the path of the flamapy src directory', 
+@command('generate_plugin', 'This command generates a new \
+         plugin to implement your cusom operations. \
+         To execute it you should set yourself in the path of the \
+         flamapy src directory', 
          ('name', str, 'The pluggins name'),
          ('extension', str, 'The extansion to be registered with the flamapy ecosystem'),
          ('path', str, 'The path to generate it'))
@@ -82,62 +84,79 @@ def generate_plugin(args):
     print("Plugin generated!")
 
 
-def flamapy_cli() -> None:
-    parser = argparse.ArgumentParser(description='FLAMA Feature Model CLI')
-    subparsers = parser.add_subparsers(dest='command')
-
-    dynamic_commands = extract_commands(FLAMAFeatureModel)
-
-    # Add dynamic commands
+def setup_dynamic_commands(subparsers, dynamic_commands):
     for name, docstring, method, parameters in dynamic_commands:
         subparser = subparsers.add_parser(name, help=docstring)
-        
-        # Add model_path to each subparser
         subparser.add_argument('model_path', type=str, help='Path to the feature model file')
-        
-        # Check if configuration_path is needed
         if 'configuration_path' in [param.name for param in parameters]:
-            subparser.add_argument('--configuration_path', type=str, help='Path to the configuration file', required=False)
-        
+            subparser.add_argument('--configuration_path', 
+                                   type=str, 
+                                   help='Path to the configuration file', 
+                                   required=False)
         for param in parameters:
             arg_name = param.name
-            if arg_name not in ['model_path', 'configuration_path']:  # Avoid duplicates
+            if arg_name not in ['model_path']:  # Avoid duplicates
                 if param.default == param.empty:  # Positional argument
-                    subparser.add_argument(arg_name, type=param.annotation, help=param.annotation.__name__)
+                    subparser.add_argument(arg_name, 
+                                           type=param.annotation, 
+                                           help=param.annotation.__name__)
                 else:  # Optional argument
-                    subparser.add_argument(f'--{arg_name}', type=param.annotation, default=param.default, help=f'Optional {param.annotation.__name__}')
+                    subparser.add_argument(f'--{arg_name}', 
+                                           type=param.annotation, 
+                                           default=param.default, 
+                                           help=f'Optional {param.annotation.__name__}')
         subparser.set_defaults(func=method, method_name=name, parameters=parameters)
 
-    # Add manually registered commands
-    for name, description, func, args in MANUAL_COMMANDS:
+
+def setup_manual_commands(subparsers, manual_commands):
+    for name, description, func, args in manual_commands:
         subparser = subparsers.add_parser(name, help=description)
         for arg in args:
             arg_name, arg_type, arg_help = arg
             subparser.add_argument(arg_name, type=arg_type, help=arg_help)
         subparser.set_defaults(func=func)
 
+
+def execute_command(args):
+    try:
+        if hasattr(args, 'method_name'):
+            cls_instance = FLAMAFeatureModel(args.model_path)
+            method_parameters = [param.name for param in args.parameters]
+            command_args = {k: v for k, v in vars(args).items() if k in method_parameters}
+            method = getattr(cls_instance, args.method_name)
+            result = method(**command_args)
+            if result is not None:
+                print(result)
+        else:
+            func = args.func
+            command_args = {k: v for k, v in vars(args).items() if k != 'func'}
+            result = func(args)
+            if result is not None:
+                print(result)
+    except FileNotFoundError as fnf_error:
+        print(f"File not found error: {fnf_error}")
+    except TypeError as type_error:
+        print(f"Type error: {type_error}")
+    except ValueError as value_error:
+        print(f"Value error: {value_error}")
+    except KeyError as key_error:
+        print(f"Key error: {key_error}")
+    except AttributeError as attr_error:
+        print(f"Attribute error: {attr_error}")
+
+
+def flamapy_cli() -> None:
+    parser = argparse.ArgumentParser(description='FLAMA Feature Model CLI')
+    subparsers = parser.add_subparsers(dest='command')
+
+    dynamic_commands = extract_commands(FLAMAFeatureModel)
+    setup_dynamic_commands(subparsers, dynamic_commands)
+    setup_manual_commands(subparsers, MANUAL_COMMANDS)
+
     args = parser.parse_args()
 
     if args.command:
-        try:
-            if hasattr(args, 'method_name'):
-                # Instantiate the class with the required parameters for each command
-                cls_instance = FLAMAFeatureModel(args.model_path, getattr(args, 'configuration_path', None))
-                method_parameters = [param.name for param in args.parameters]
-                command_args = {k: v for k, v in vars(args).items() if k in method_parameters}
-                method = getattr(cls_instance, args.method_name)
-                result = method(**command_args)
-                if result is not None:
-                    print(result)
-            else:
-                # Call the manually registered command
-                func = args.func
-                command_args = {k: v for k, v in vars(args).items() if k != 'func'}
-                result = func(args)
-                if result is not None:
-                    print(result)
-        except Exception as e:
-            print(f"Error: {e}")
+        execute_command(args)
     else:
         print("Feature model operations:")
         for name, docstring, _, _ in dynamic_commands:
--- flamapy/interfaces/python/flamapy_feature_model.py
+++ flamapy/interfaces/python/flamapy_feature_model.py
@@ -1,24 +1,20 @@
 from flamapy.core.discover import DiscoverMetamodels
 from flamapy.metamodels.fm_metamodel.models import FeatureModel
-from typing import Optional
+from flamapy.core.exceptions import FlamaException
 
-class FLAMAFeatureModel():
 
-    def __init__(self, model_path:str, configuration_path:Optional[str]=None):
+class FLAMAFeatureModel: 
+
+    def __init__(self, model_path: str):
         """
         This is the path in the filesystem where the model is located. 
         Any model in UVL, FaMaXML or FeatureIDE format are accepted
         """
         self.model_path = model_path
         """
-        This is the path in the filesystem where the configuration is located. 
-        Only CSV format are accepted (see documentation for more information)
-        """
-        self.configuration_path = configuration_path
-        """
         Creating the interface witht he flama framework
         """
-        self.dm = DiscoverMetamodels()
+        self.discover_metamodel = DiscoverMetamodels()
         """
         We save the model for later ussage
         """
@@ -29,17 +25,16 @@ class FLAMAFeatureModel():
         self.sat_model = None
         self.bdd_model = None
 
+    def _read(self, model_path) -> FeatureModel:
+        return self.discover_metamodel.use_transformation_t2m(model_path, 'fm')
 
-    def _read(self, model_path)->FeatureModel:
-        return self.dm.use_transformation_t2m(model_path,'fm')
-    
     def _transform_to_sat(self):
         if self.sat_model is None:
-            self.sat_model=self.dm.use_transformation_m2m(self.fm_model,"pysat")
-    
+            self.sat_model = self.discover_metamodel.use_transformation_m2m(self.fm_model, "pysat")
+
     def _transform_to_bdd(self):
         if self.bdd_model is None:
-            self.bdd_model=self.dm.use_transformation_m2m(self.fm_model,"bdd")
+            self.bdd_model = self.discover_metamodel.use_transformation_m2m(self.fm_model, "bdd")
 
     def atomic_sets(self):
         """ 
@@ -51,7 +46,8 @@ class FLAMAFeatureModel():
 
         # Try to use the Find operation, which returns the atomic sets if they are found
         try:
-            atomic_sets = self.dm.use_operation(self.fm_model,'FMAtomicSets').get_result()
+            atomic_sets = self.discover_metamodel.use_operation(self.fm_model,
+                                                                'FMAtomicSets').get_result()
             result = []
             for atomic_set in atomic_sets:
                 partial_set = []
@@ -59,11 +55,10 @@ class FLAMAFeatureModel():
                     partial_set.append(feature.name)
                 result.append(partial_set)
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-        
     def average_branching_factor(self):
         """ 
         This refers to the average number of child features that a parent feature has in a 
@@ -75,13 +70,13 @@ class FLAMAFeatureModel():
 
         # Try to use the Find operation, which returns the atomic sets if they are found
         try:
-            result = self.dm.use_operation(self.fm_model,'FMAverageBranchingFactor').get_result()
+            result = self.discover_metamodel.use_operation(self.fm_model,
+                                                           'FMAverageBranchingFactor').get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-        
     def count_leafs(self):
         """ 
         This operation counts the number of leaf features in a feature model. Leaf features 
@@ -91,13 +86,13 @@ class FLAMAFeatureModel():
 
         # Try to use the Find operation, which returns the atomic sets if they are found
         try:
-            result = self.dm.use_operation(self.fm_model,'FMCountLeafs').get_result()
+            result = self.discover_metamodel.use_operation(self.fm_model,
+                                                           'FMCountLeafs').get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-        
     def estimated_number_of_configurations(self):
         """ 
          This is an estimate of the total number of different products that can be produced 
@@ -109,14 +104,14 @@ class FLAMAFeatureModel():
 
         # Try to use the Find operation, which returns the atomic sets if they are found
         try:
-            result = self.dm.use_operation(self.fm_model,'FMEstimatedConfigurationsNumber').get_result()
+            result = self.discover_metamodel.use_operation(
+                self.fm_model, 'FMEstimatedConfigurationsNumber').get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def feature_ancestors(self,feature_name:str):
+    def feature_ancestors(self, feature_name: str):
         ''' 
         These are the features that are directly or indirectly the parent of a given feature in 
         a feature model. Ancestors of a feature are found by traversing up the feature hierarchy. 
@@ -124,7 +119,7 @@ class FLAMAFeatureModel():
         '''
         # Try to use the Find operation, which returns the atomic sets if they are found
         try:
-            operation = self.dm.get_operation(self.fm_model,'FMFeatureAncestors')
+            operation = self.discover_metamodel.get_operation(self.fm_model, 'FMFeatureAncestors')
             operation.set_feature(self.fm_model.get_feature_by_name(feature_name))
             operation.execute(self.fm_model)
             flama_result = operation.get_result()
@@ -132,11 +127,9 @@ class FLAMAFeatureModel():
             for res in flama_result:
                 result.append(res.name)
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
-
-
 
     def leaf_features(self):
         """ 
@@ -144,24 +137,24 @@ class FLAMAFeatureModel():
         It returns the leaf features if they are found in the model. 
         If the model does not follow the UVL specification, an 
         exception is raised and the operation returns False.
-        
+
         Traditionally you would use the flama tool by             
-        features = dm.use_operation_from_file('OperationString', model)
+        features = discover_metamodel.use_operation_from_file('OperationString', model)
         however, in this tool we know that this operation is from the fm metamodel, 
         so we avoid to execute the transformation if possible
         """
 
         # Try to use the operation, which returns the leaf features if they are found
         try:
-            features = self.dm.use_operation(self.fm_model,'FMLeafFeatures').get_result()
+            features = self.discover_metamodel.use_operation(self.fm_model,
+                                                             'FMLeafFeatures').get_result()
             leaf_features = []
             for feature in features:
                 leaf_features.append(feature.name)
             return leaf_features
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
-
 
     def max_depth(self):
         """ 
@@ -173,31 +166,30 @@ class FLAMAFeatureModel():
 
         # Try to use the Find operation, which returns the max depth of the tree
         try:
-            return self.dm.use_operation(self.fm_model,'FMMaxDepthTree').get_result()
-        except Exception as e:
-            print(f"Error: {e}")
+            return self.discover_metamodel.use_operation(self.fm_model,
+                                                         'FMMaxDepthTree').get_result()
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-    """
-    The methods above rely on sat to be executed.
-    """
-
+    #The methods above rely on sat to be executed.
     def core_features(self):
         """
-        These are the features that are present in all products of a product line. In a feature model, 
-        they are the features that are mandatory and not optional. Core features define the commonality 
-        among all products in a product line. This call requires sat to be called, however, there is 
-        an implementation within flama that does not requires sat. please use the framework in case of needing it. 
+        These are the features that are present in all products of a product line. 
+        In a feature model, they are the features that are mandatory and not optional. 
+        Core features define the commonality among all products in a product line. 
+        This call requires sat to be called, however, there is an implementation within
+        flamapy that does not requires sat. please use the framework in case of needing it. 
         """
         try:
             self._transform_to_sat()
-            features = self.dm.use_operation(self.sat_model,'PySATCoreFeatures').get_result()
+            features = self.discover_metamodel.use_operation(self.sat_model,
+                                                             'PySATCoreFeatures').get_result()
             return features
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-        
     def dead_features(self):
         """
         These are features that, due to the constraints and dependencies in the 
@@ -206,12 +198,12 @@ class FLAMAFeatureModel():
         """
         try:
             self._transform_to_sat()
-            features = self.dm.use_operation(self.sat_model,'PySATDeadFeatures').get_result()
+            features = self.discover_metamodel.use_operation(self.sat_model,
+                                                             'PySATDeadFeatures').get_result()
             return features
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
-
 
     def false_optional_features(self):
         """
@@ -221,78 +213,83 @@ class FLAMAFeatureModel():
         """
         try:
             self._transform_to_sat()
-
-            operation = self.dm.get_operation(self.sat_model,'PySATFalseOptionalFeatures')
-            operation.feature_model=self.fm_model
+            operation = self.discover_metamodel.get_operation(self.sat_model,
+                                                              'PySATFalseOptionalFeatures')
+            operation.feature_model = self.fm_model
             operation.execute(self.sat_model)
             features = operation.get_result()
             return features
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def filter(self, configurationPath:str):
+    def filter(self, configuration_path: str):
         """
-        This operation selects a subset of the products of a product line based on certain criteria. 
-        For example, you might filter the products to only include those that contain a certain feature.
+        This operation selects a subset of the products of a product line based on certain 
+        criteria. For example, you might filter the products to only include those that 
+        contain a certain feature.
         """
         try:
             self._transform_to_sat()
-            configuration = self.dm.use_transformation_t2m(configurationPath,'configuration')
-
-            operation = self.dm.get_operation(self.sat_model,'PySATFilter')
+            configuration = self.discover_metamodel.use_transformation_t2m(configuration_path,
+                                                                           'configuration')
+            operation = self.discover_metamodel.get_operation(self.sat_model, 'PySATFilter')
             operation.set_configuration(configuration)
             operation.execute(self.sat_model)
             result = operation.get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def configurations_number(self, with_sat:bool=False):
+    def configurations_number(self, with_sat: bool = False):
         """
-        This is the total number of different full configurations that can be produced from a feature model. 
-        It's calculated by considering all possible combinations of features, taking into account 
-        the constraints and dependencies between features.
+        This is the total number of different full configurations that can be 
+        produced from a feature model. It's calculated by considering all possible 
+        combinations of features, taking into account the constraints and 
+        dependencies between features.
         """
         try:
-            nop=0
+            nop = 0
             if with_sat:
                 self._transform_to_sat()
-                nop = self.dm.use_operation(self.sat_model,'PySATConfigurationsNumber').get_result()
-                
+                nop = self.discover_metamodel.use_operation(
+                    self.sat_model, 'PySATConfigurationsNumber'
+                ).get_result()
             else:
                 self._transform_to_bdd()
-                nop = self.dm.use_operation(self.bdd_model,'BDDConfigurationsNumber').get_result()
+                nop = self.discover_metamodel.use_operation(
+                    self.bdd_model, 'BDDConfigurationsNumber'
+                ).get_result()
             return nop
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def configurations(self, with_sat:bool=False):
+    def configurations(self, with_sat: bool = False):
         """
         These are the individual outcomes that can be produced from a feature model. Each product 
         is a combination of features that satisfies all the constraints and dependencies in the 
         feature model.
         """
         try:
-            products=[]
+            products = []
             if with_sat:
                 self._transform_to_sat()
-                products = self.dm.use_operation(self.sat_model,'PySATConfigurations').get_result()
+                products = self.discover_metamodel.use_operation(self.sat_model,
+                                                                 'PySATConfigurations'
+                                                                 ).get_result()
             else:
                 self._transform_to_bdd()
-                products = self.dm.use_operation(self.bdd_model,'BDDConfigurations').get_result()
+                products = self.discover_metamodel.use_operation(self.bdd_model,
+                                                                 'BDDConfigurations'
+                                                                 ).get_result()
             return products
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def commonality(self, configurationPath:str):
+    def commonality(self, configuration_path: str):
         """
         This is a measure of how often a feature appears in the products of a 
         product line. It's usually expressed as a percentage. A feature with 
@@ -300,39 +297,40 @@ class FLAMAFeatureModel():
         """
         try:
             self._transform_to_sat()
-            configuration = self.dm.use_transformation_t2m(configurationPath,'configuration')
+            configuration = self.discover_metamodel.use_transformation_t2m(configuration_path,
+                                                                           'configuration')
 
-            operation = self.dm.get_operation(self.sat_model,'PySATCommonality')
+            operation = self.discover_metamodel.get_operation(self.sat_model, 'PySATCommonality')
             operation.set_configuration(configuration)
             operation.execute(self.sat_model)
             return operation.get_result()
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
 
-
-    def satisfiable_configuration(self, configurationPath:str, full_configuration:bool=False):
+    def satisfiable_configuration(self, configuration_path: str, full_configuration: bool = False):
         """
         This is a product that is produced from a valid configuration of features. A valid 
         product satisfies all the constraints and dependencies in the feature model.
         """
         try:
             self._transform_to_sat()
-            configuration = self.dm.use_transformation_t2m(configurationPath,'configuration')
-            operation = self.dm.get_operation(self.sat_model,'PySATSatisfiableConfiguration')
-            print(operation)
+            configuration = self.discover_metamodel.use_transformation_t2m(configuration_path, 
+                                                                           'configuration')
+            operation = self.discover_metamodel.get_operation(self.sat_model, 
+                                                              'PySATSatisfiableConfiguration')
+
             if full_configuration:
                 operation.set_configuration(configuration, is_full=True)
             else:
                 operation.set_configuration(configuration, is_full=False)
-                
+
             operation.execute(self.sat_model)
             result = operation.get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
-
 
     def satisfiable(self):
         """
@@ -342,10 +340,9 @@ class FLAMAFeatureModel():
         """
         try:
             self._transform_to_sat()
-            result = self.dm.use_operation(self.sat_model,'PySATSatisfiable').get_result()
+            result = self.discover_metamodel.use_operation(self.sat_model,
+                                                           'PySATSatisfiable').get_result()
             return result
-        except Exception as e:
-            print(f"Error: {e}")
+        except FlamaException as exception:
+            print(f"Error: {exception}")
             return None
-
-
